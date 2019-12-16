80c25e229f8f499c10bb830e84786f457b9ca10c
Ryan Rottmann <rfrfkb@mail.missouri.edu>
2019-12-16 15:45:01 -0600

    Output fix

--- Pets/Program.cs
+++ Pets/Program.cs
@@ -7,30 +7,26 @@ namespace Pets
     {
         static void Main(string[] args)
         {
-
             Pet pet1 = new Pet("dog", "Shadow", "Jose", 42.6);
             Console.WriteLine("Name: " + pet1.name);
             Console.WriteLine("Weight: " + pet1.weight);
-            Console.WriteLine(pet1.getTag());
-
-            Console.WriteLine("");
+            Console.WriteLine(pet1.getTag() + "\n");
 
             Dog dog1 = new Dog("Daisy", "Kent", 23.4);
 
             Console.WriteLine("Name: " + dog1.name);
             Console.WriteLine("Weight: " + dog1.weight);
             Console.WriteLine(dog1.getTag());
-            Console.WriteLine(dog1.Bark(4));
-
-            Console.WriteLine("");
+            Console.WriteLine(dog1.Bark(4) + "\n");
 
             Cat cat1 = new Cat("Simba", "Maria", 5.2);
 
             Console.WriteLine("Name: " + cat1.name);
             Console.WriteLine("Weight: " + cat1.weight);
             Console.WriteLine(cat1.getTag());
-            Console.WriteLine(cat1.Meow(4));
-
+            Console.WriteLine(cat1.Meow(4) + "\n");
+            Console.WriteLine("End of program\nPress any key to close");
+            Console.ReadKey();
         }
     }
 }
